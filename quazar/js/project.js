8fe59760a509fbc41bc7fb79cbe9d9105f359c40
Quazarquay <nickcuellar@yahoo.com>
2015-09-10 22:32:57 -0400

    background and more melee page

---
+++ quazar/js/project.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$(".menu-toggle").click(function(){
+		$("nav").fadeToggle(800);
+		// $(".menu-toggle").fadeToggle(300);
+	});
+});
+
+// $(window).resize(function(){
+// 	if($(window).width() > 500) {
+// 		$("nav").attr("style", "");
+// 	}
+// });
+
